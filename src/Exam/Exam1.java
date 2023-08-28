480510f330ce57ccc6cb5ad1f472f66775534f99
Jsb-github <tjdqls2021@gmail.com>
2023-08-28 17:03:50 +0900

    0

---
+++ src/Exam/Exam1.java
@@ -0,0 +1,27 @@
+package Exam;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        /*
+        *  간단한 논리 연산
+        * 문제 설명
+        boolean 변수 x1, x2, x3, x4가 매개변수로 주어질 때,
+        다음의 식의 true/false를 return 하는 solution 함수를 작성해 주세요.
+        *
+        * */
+        /*
+        boolean x1  = false;
+        boolean x2 = true;
+        boolean x3 = true;
+        boolean x4 = true;
+        boolean answer = true;
+        if(x1 == true || x2==true || x3==true || x4==true){
+            answer =true;
+        }else{
+            answer = false;
+        }
+        System.out.println("answer = " + answer);
+        */
+
+    }
+}
