ac86e0337a6ac72085e06c82516e45507d4923a0
Jsb-github <tjdqls2021@gmail.com>
2023-08-28 15:33:06 +0900

    객체와 클래스

---
+++ src/ch06class/C01class.java
@@ -0,0 +1,37 @@
+package ch06class;
+
+public class C01class {
+    public static void main(String[] args) {
+        MyClass01 o1 = new MyClass01();
+
+        System.out.println(System.identityHashCode(o1));
+
+
+        System.out.println("ol.age =" + o1.age);
+
+        o1.age = 30;
+
+        System.out.println("o1 = " + o1.age);
+
+        MyClass01 o2 = o1;
+
+        System.out.println("o2.age = " + o2.age);
+
+        o1.age=60;
+
+        System.out.println("o2 = " + o2.age);
+
+    }
+}
+
+
+class MyClass01 {
+    // instance fields
+    //인스턴스가 가져하는 속성들
+    int age;
+
+    //intance methods
+    //인스터가 가져하는 기능들
+
+
+}
