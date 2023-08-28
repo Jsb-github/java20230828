405859fcb3a0bbc3d4e6bc9ba60def04df0e4ab4
Jsb-github <tjdqls2021@gmail.com>
2023-08-28 11:43:19 +0900

    first

---
+++ src/Ch05reference/C01primitive.java
@@ -0,0 +1,35 @@
+package Ch05reference;
+
+public class C01primitive {
+	public static void main(String[] args) {
+		int a; // 변수 선언
+		a = 10; //데이터타입(자료형)에 맞는 값만 대입 가능
+		//a = 3.14// 데이터타입에 맞지 않다면 대입 불가
+	
+		int b=5; //선언과 동시에 값 대입
+		
+		//기본 타입 8개
+		// 정수 형
+		// byte(1byte) short(2byte)  int(4byte) long(8byte)
+		
+		// 실수 형
+		// float(4byte) double(8byte)
+		
+		// 논리 형
+		// boolean (1byte)
+		
+		// 문자 형
+		// char (2byte)
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
