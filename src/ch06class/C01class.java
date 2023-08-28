63e86c54f44fefb4e3882ac062fcfa5ab49f1129
Jsb-github <tjdqls2021@gmail.com>
2023-08-28 16:47:08 +0900

    Myclass01에 address 필드 추가

--- src/ch06class/C01class.java
+++ src/ch06class/C01class.java
@@ -5,18 +5,13 @@ public class C01class {
         MyClass01 o1 = new MyClass01();
 
         System.out.println(System.identityHashCode(o1));
-
-
         System.out.println("ol.age =" + o1.age);
-
         o1.age = 30;
 
         System.out.println("o1 = " + o1.age);
-
         MyClass01 o2 = o1;
 
         System.out.println("o2.age = " + o2.age);
-
         o1.age=60;
 
         System.out.println("o2 = " + o2.age);
